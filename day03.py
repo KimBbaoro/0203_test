d9aeb85a97d2607072b51104438108c3758cc791
kimkunyou <bbaro1435@gmail.com>
2023-01-16 16:06:20 +0900

    p1

---
+++ day03.py
@@ -0,0 +1,11 @@
+letter = '''
+        Dear {salutation} {name},
+        Thank you for your letter. We are sorry that out {product} {verbed} in your {room}. Please note that it should never be used in a {room}, especially near any {animals}.
+
+        Send us your reciept and {amount} for shipping and handling. We will send you another {product} that, in our tests, is {percent}% less to have {verbed}.
+
+        Thank you for you support.
+        Sicerely,
+        {spokesman}
+        {job_title}
+'''

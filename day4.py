3cee4b3b5e0daea68c5649f8091f03e92cf815ba
kimkunyou <bbaro1435@gmail.com>
2023-01-17 16:24:28 +0900

    day04 08

--- day4.py
+++ day4.py
@@ -1,17 +1,1 @@
-start1 = ["fee", "fie", "foe"]
-rhymes = [
-    ("flop", "get a mop"),
-    ("flop", "turn the rope"),
-    ("fa", "get your ma"),
-    ("fudge", "call the judge"),
-    ("fat", "pet the cat"),
-    ("fog", "walk the dog"),
-    ("fun", "say we're done"),
-]
-start2 = "Someone better"
-
-for start_1 in start1:
-    print(start_1.capitalize() + '!', end=' ')
-print()
-print(start2 + ' ', end = ' ')
-print(rhymes[0][1])
+e2f = {"dog" : "chien", "cat" : "chat", "walrus" : "morse"}

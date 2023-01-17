a1506579a2e1ec8294254e9eca3d75b93a1dd4d7
kimkunyou <bbaro1435@gmail.com>
2023-01-17 16:22:11 +0900

    day04 07

--- day4.py
+++ day4.py
@@ -1,2 +1,17 @@
-even = [num for num in range(0,10,2)]
-print(even)
+start1 = ["fee", "fie", "foe"]
+rhymes = [
+    ("flop", "get a mop"),
+    ("flop", "turn the rope"),
+    ("fa", "get your ma"),
+    ("fudge", "call the judge"),
+    ("fat", "pet the cat"),
+    ("fog", "walk the dog"),
+    ("fun", "say we're done"),
+]
+start2 = "Someone better"
+
+for start_1 in start1:
+    print(start_1.capitalize() + '!', end=' ')
+print()
+print(start2 + ' ', end = ' ')
+print(rhymes[0][1])

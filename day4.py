10507544f8e8a577311b0edebade0cde8744ac8c
kimkunyou <bbaro1435@gmail.com>
2023-01-17 16:14:44 +0900

    day04 06

--- day4.py
+++ day4.py
@@ -1,7 +1,2 @@
-surprise = ["Groucho", "Chico", "Harpo"]
-target = surprise[-1]
-target = target.lower()
-target = target[::-1]
-target = target.capitalize()
-surprise[-1] = target
-print(surprise)
+even = [num for num in range(0,10,2)]
+print(even)

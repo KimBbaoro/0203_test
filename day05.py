9584a79eea69ebcf0afc53d17dd857e5b6848d56
kimkunyou <bbaro1435@gmail.com>
2023-01-18 16:15:32 +0900

    day05 p3

--- day05.py
+++ day05.py
@@ -1,4 +1,6 @@
-def good():
-    return ["Harry", "Ron", "hermione"]
+def get_odds():
+    return [odd_num for odd_num in range(1,10,2)]
 
-print(good())
+for i in range(len(get_odds())):
+    if i == 2:
+        print(get_odds()[i])

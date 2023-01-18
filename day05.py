c5b0226304a072583093e7ba5b006a87fc7226c7
kimkunyou <bbaro1435@gmail.com>
2023-01-18 17:44:21 +0900

    day05 p3 modified

--- day05.py
+++ day05.py
@@ -1,6 +1,15 @@
-def get_odds():
-    return [odd_num for odd_num in range(1,10,2)]
+test = [num for num in range(10)]
+print(type(i for i in range(10)))
 
-for i in range(len(get_odds())):
-    if i == 2:
-        print(get_odds()[i])
+def odd_nums():
+    return (odd_num for odd_num in range(1,10,2))
+
+# for i in range(len(list(odd_nums()))):
+#     if i == 2:
+#         print(list(odd_nums())[2])
+
+for i,j in enumerate(odd_nums()):
+    if i ==2:
+        print(j)
+
+

60ae44df4567f8d1a9910258f75f1424f3b6ca2c
kimkunyou <bbaro1435@gmail.com>
2023-01-20 16:27:47 +0900

    day07 p7

--- day07.py
+++ day07.py
@@ -9,13 +9,28 @@ class Element:
         print(self.symbol)
         print(self.number)
 
+class Element2:
+    def __init__(self, name, symbol, number):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
+
+    def __str__(self):
+        return f'{self.name}, {self.symbol}, {self.number}'
+        # print(self.name)
+        # print(self.symbol)
+        # print(self.number)
+
 
 #element = Element("Hydrogen","H",1)
 
 el_dict = {'name':"Hydrogen", "symbol" : "H", "number" : 1}
 hydrogen = Element(el_dict['name'],el_dict['symbol'],el_dict['number'])
+hydrogen2 = Element2(el_dict['name'],el_dict['symbol'],el_dict['number'])
+print("element1 : ",hydrogen)
+print("element2 : ",hydrogen2)
 
-hydrogen.dump()
+
 
 
 

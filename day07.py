c3130a1233318c57d3dd42133cc134c2862d212d
kimkunyou <bbaro1435@gmail.com>
2023-01-20 16:19:56 +0900

    day07 p6

--- day07.py
+++ day07.py
@@ -4,10 +4,18 @@ class Element:
         self.symbol = symbol
         self.number = number
 
+    def dump(self):
+        print(self.name)
+        print(self.symbol)
+        print(self.number)
+
+
 #element = Element("Hydrogen","H",1)
 
 el_dict = {'name':"Hydrogen", "symbol" : "H", "number" : 1}
 element = Element(el_dict['name'],el_dict['symbol'],el_dict['number'])
+
+element.dump()
 
 
 

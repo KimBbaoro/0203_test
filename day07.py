faceb835209b9da86c7dcb1490369d98798eae48
kimkunyou <bbaro1435@gmail.com>
2023-01-20 16:37:40 +0900

    day07 p9

--- day07.py
+++ day07.py
@@ -1,42 +1,16 @@
-class Element:
-    def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
+class Bear:
+    def eats(self):
+        return 'Berries'
 
-    def dump(self):
-        print(self.name)
-        print(self.symbol)
-        print(self.number)
+class Rabbit:
+    def eats(self):
+        return "Clover"
 
-class Element2:
-    def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
+class Octothorpe:
+    def eats(self):
+        return 'campers'
 
-    @property
-    def infomation(self):
-        return [self.name, self.symbol, self.number]
-
-    @infomation.setter
-    def information(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
-
-
-
-
-#element = Element("Hydrogen","H",1)
-
-el_dict = {'name':"Hydrogen", "symbol" : "H", "number" : 1}
-hydrogen = Element(el_dict['name'],el_dict['symbol'],el_dict['number'])
-hydrogen2 = Element2(el_dict['name'],el_dict['symbol'],el_dict['number'])
-print("element1 : ",hydrogen)
-print("element2 : ",hydrogen2)
-
-
-
-
-
+a = Bear()
+b = Rabbit()
+c = Octothorpe()
+print(a.eats(),b.eats(),c.eats())
